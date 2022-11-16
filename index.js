bfc5571faf258942189021939f3f9f4280c23585
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-16 20:41:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+//add
+function add(a, b) {
+    return a + b
+  }
+  //subtract
+  function subtract(a, b) {
+    return a - b
+  }
+  //multiply
+  function multiply(a, b) {
+    return a * b
+  }
+  //divide
+  function divide(a, b) {
+    return a / b
+  }
+  //increment
+  function increment(n) {
+    return n + 1
+  }
+  //decrement
+  function decrement(n) {
+    return n - 1
+  }
+  //parseint
+  function makeInt(n) {
+    return parseInt(n,10 )
+  }
+  //parseFloat
+  function preserveDecimal(n) {
+    return parseFloat(n)
+  }
